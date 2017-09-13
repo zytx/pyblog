500482678b13e283710ba60bcdfd7c6063a3aef1
BlueDream <zywsad@gmail.com>
2017-09-13 08:34:51 +0000

    文章非站内链接自动添加nofollow

--- pyblog/templatetags/pyblog.py
+++ pyblog/templatetags/pyblog.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 import mistune
 
 register = template.Library()
@@ -20,6 +21,40 @@ class Renderer(mistune.Renderer):
         :param raw: raw text content of the header.
         """
         return '<h%d class="h%d my-4">%s</h%d>\n' % (level, level+2 ,text, level)
+
+    def autolink(self, link, is_email=False):
+        """Rendering a given link or email address.
+
+        :param link: link content or email address.
+        :param is_email: whether this is an email or not.
+        """
+        text = link = mistune.escape(link)
+
+        if is_email:
+            link = 'mailto:%s' % link
+        if settings.DOMAIN_NAME in link:
+            return '<a href="%s">%s</a>' % (link, text)
+        else:
+            return '<a href="%s" rel="external nofollow" target="_blank">%s</a>' % (link, text)
+
+    def link(self, link, title, text):
+        """Rendering a given link with content and title.
+
+        :param link: href link for ``<a>`` tag.
+        :param title: title content for `title` attribute.
+        :param text: text content for description.
+        """
+        link = mistune.escape_link(link)
+        if settings.DOMAIN_NAME in link:
+            if not title:
+                return '<a href="%s">%s</a>' % (link, text)
+            title = mistune.escape(title, quote=True)
+            return '<a href="%s" title="%s">%s</a>' % (link, title, text)
+        else:
+            if not title:
+                return '<a href="%s" rel="external nofollow" target="_blank">%s</a>' % (link, text)
+            title = mistune.escape(title, quote=True)
+            return '<a href="%s" title="%s" rel="external nofollow" target="_blank">%s</a>' % (link, title, text)
 
 
 @register.filter()
